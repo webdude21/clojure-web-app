0f4f3282a2efa8167fd8d1ddeb929615e19c6cd7
webdude <dimo.petrof@gmail.com>
2017-06-18 12:12:59 +0300

    prepare to deploy on heroku

--- project.clj
+++ project.clj
@@ -4,9 +4,15 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
+                 [clj-http "3.6.1"]
+                 [environ "1.1.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-environ "1.1.0"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojure-web-app-SNAPSHOT-standalone.jar"
   :ring {:handler clojure-web-app.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/clojure_web_app/handler.clj
+++ src/clojure_web_app/handler.clj
@@ -6,13 +6,33 @@
             [ring.middleware.json :refer [wrap-json-params]]
             [ring.util.response :refer [response status]]
             [compojure.route :as route]
+            [clojure.data.json :as json]
+            [clj-http.client :as client]
+            [environ.core :refer [env]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn location-by-ip [ip]
+  (json/read-str ((client/get (format "http://freegeoip.net/json/%s" "github.com")) :body) :key-fn keyword))
+
+(defn nearby-fuel-prices [lat lon limit distance fuel]
+  (json/read-str
+    ((client/get "http://fuelo.net/api/near" {:query-params {:key      (env :fuelo-api-key)
+                                                             :lat      lat
+                                                             :lon      lon
+                                                             :limit    limit
+                                                             :distance distance
+                                                             :fuel     fuel}}) :body)))
 (defroutes app-routes
            (GET "/user/:id" [id greeting]
              {:body {:userId   id
                      :greeting greeting}})
            (GET "/print-query-params" [& args] (response args))
+           (GET "/cheapest-near-me" [limit distance fuel]
+             (fn [request]
+               (let [location (location-by-ip (:remote-addr request))]
+                 (response (nearby-fuel-prices (location :latitude) (location :longitude) limit distance fuel)))))
+           (GET "/my-location" []
+             (fn [request] (response (location-by-ip (:remote-addr request)))))
            (GET "/my-ip" []
              (fn [request]
                {:status 200
