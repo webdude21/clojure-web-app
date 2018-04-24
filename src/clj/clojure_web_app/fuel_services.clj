2237d9d6e0d0f86993880e2b9875b1932457624f
webdude <dimo.petrof@gmail.com>
2018-04-24 07:39:32 +0300

    reformatted

--- src/clj/clojure_web_app/fuel_services.clj
+++ src/clj/clojure_web_app/fuel_services.clj
@@ -12,10 +12,12 @@
 
 (defn nearby-fuel-prices [lat lon limit distance fuel ip]
   (json/read-str
-    (let [apiResult (client/get "http://fuelo.net/api/near" {:query-params {:key      (env :fuelo-api-key)
-                                                                            :lat      (or lat (:latitude (location-by-ip ip)))
-                                                                            :lon      (or lon (:longitude (location-by-ip ip)))
-                                                                            :limit    (or limit "10")
-                                                                            :distance (or distance "10")
-                                                                            :fuel     (or fuel "lpg")}})]
+    (let [apiResult (client/get
+                      "http://fuelo.net/api/near"
+                      {:query-params {:key      (env :fuelo-api-key)
+                                      :lat      (or lat (:latitude (location-by-ip ip)))
+                                      :lon      (or lon (:longitude (location-by-ip ip)))
+                                      :limit    (or limit "10")
+                                      :distance (or distance "10")
+                                      :fuel     (or fuel "lpg")}})]
       (apiResult :body))))
